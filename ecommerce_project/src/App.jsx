fa6772abc8c14e3aee88155f96dae995da25cb42
Nandinibilla <billanandini14@gmail.com>
2025-03-26 15:25:53 +0530

    App.jsx

--- ecommerce_project/src/App.jsx
+++ ecommerce_project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <nav className="category-heading">
     <nav className="navbar navbar-expand-lg bg-body-teritary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"><img src="https://sellerseva.com/wp-content/uploads/2022/11/Ajio-Lofo.png" height="50px" width="100px"></img></a>
+  
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
